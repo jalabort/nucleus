a18a1b92dbfec5ef20cc88bb1939b5af059692b7
Joan Alabort <joan.alabort@hudl.com>
2019-09-02 09:04:37 +0100

    Genral clean up

--- setup.py
+++ setup.py
@@ -6,28 +6,28 @@ setup(
     version='0.0.1',
     author='hudlrd',
     install_requires=[
-        'stringcase==1.2.0',
-        'tqdm==4.31.1',
-        'boto3==1.9.114',
+        'stringcase>=1.2.0',
+        'tqdm>=4.32.2',
+        'boto3>=1.9.114',
 
-        'altair==2.4.1',
-        'matplotlib==3.0.3',
-        'pydot==1.4.1',
-        'graphviz==0.10.1',
+        'altair>=3.1.0',
+        'matplotlib>=3.0.3',
+        'pydot>=1.4.1',
+        'graphviz>=0.10.1',
 
-        'pillow==5.4.1',
+        'pillow>=5.4.1',
 
         'quilt==2.9.15',
 
-        'pandas==0.24.1',
-        'tensorflow-addons==0.4.0',
+        'pandas>=0.24.2',
+        'tensorflow-addons>=0.5.0',
 
         # Hudl packages
-        'hudl-aws==0.2.1'
+        'hudl-aws>=0.2.5'
     ],
     extras_require={
-        'cpu': ['tensorflow==2.0.0-beta1'],
-        'gpu': ['tensorflow-gpu==2.0.0-beta1']
+        'cpu': ['tensorflow==2.0.0-rc0'],
+        'gpu': ['tensorflow-gpu==2.0.0-rc0']
     },
     test_require={
         'pytest',
